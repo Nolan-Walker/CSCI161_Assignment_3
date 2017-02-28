08bc50597b10f34e7fc3fd7398023d78c0dcd45d
Nolan Caissie <nolancaissie@nolans-macbook-air.local>
2017-02-28 13:45:33 -0800

    Initial Commit

---
+++ assignment 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment 3
+//
+//  Created by Nolan Caissie on 2017-02-28.
+//  Copyright © 2017 Nolan Caissie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
